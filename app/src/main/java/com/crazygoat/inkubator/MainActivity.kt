e7d3e742835a86b578c2390c729d04be28445d1d
Piotr Hałas <Piotr.Halas@komputronik.pl>
2019-06-26 15:19:36 +0200

    poprawki

--- app/src/main/java/com/crazygoat/inkubator/MainActivity.kt
+++ app/src/main/java/com/crazygoat/inkubator/MainActivity.kt
@@ -2,22 +2,45 @@ package com.crazygoat.inkubator
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import android.hardware.ConsumerIrManager
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import java.util.logging.Logger
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var manager: ConsumerIrManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        val coordinatorLayout = findViewById(R.id.coordinatorLayout) as CoordinatorLayout
+
+        this.manager = getSystemService(CONSUMER_IR_SERVICE) as ConsumerIrManager
+        if (this.manager.hasIrEmitter()) {
+            val ranges = this.manager.carrierFrequencies
+
+            Snackbar.make(coordinatorLayout, "IR DEVICE FOUND", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+            ranges.forEach {
+                Logger.getLogger(MainActivity::class.java.name).warning(it.minFrequency.toString() +" = "+it.maxFrequency.toString())
+            }
+        } else {
+            Snackbar.make(coordinatorLayout, "IR DEVICE NOT FOUND", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+        }
+
+
+        plus.setOnClickListener { view ->
+            Snackbar.make(view, "Plus clicked", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+        }
+        minus.setOnClickListener { view ->
+            Snackbar.make(view, "Minus clicked", Snackbar.LENGTH_LONG).setAction("Action", null).show()
         }
     }
 
