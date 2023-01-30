97b3c12e7b67c9e5e0cab58ff9b5965933b64de0
Piotr Hałas <piohal@boozt.com>
2023-01-30 09:46:46 +0100

    Simplify code

--- incubator/src/main/java/com/crazygoat/inkubator/MainActivity.kt
+++ incubator/src/main/java/com/crazygoat/inkubator/MainActivity.kt
@@ -3,15 +3,12 @@ package com.crazygoat.inkubator
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.hardware.ConsumerIrManager
 import android.net.Uri
-import android.os.Build
-import android.os.Bundle
-import android.os.VibrationEffect
-import android.os.Vibrator
+import android.os.*
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
@@ -21,23 +18,35 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
     private var plusIRMsg = intArrayOf(907, 814, 4376, 813, 904)
     private var minusIRMsg = intArrayOf(1771, 821, 3499, 823, 869)
     private var selectedFreq = 0
     private var freqList = mutableListOf<Int>()
+    private var prefs: SharedPreferences? = null
     private lateinit var manager: ConsumerIrManager
     private lateinit var mAdView: AdView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        prefs = getSharedPreferences(packageName, MODE_PRIVATE)
 
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         initAds()
         initIR()
         registerButtons()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (prefs!!.getBoolean(getString(R.string.first_run), true)) {
+            showHelpDialog()
+            with(prefs!!.edit()) {
+                putBoolean(getString(R.string.first_run), false)
+                apply()
+            }
+        }
     }
 
     private fun registerButtons() {
@@ -57,18 +66,18 @@ class MainActivity : AppCompatActivity() {
     private fun initAds() {
         MobileAds.initialize(this)
         mAdView = findViewById(R.id.adView)
-        val adRequest = AdRequest.Builder()
-
-        mAdView.loadAd(adRequest.build())
+        @SuppressWarnings("VisibleForTests")
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 
     private fun displayNoIRWarning() {
         Snackbar.make(
             findViewById(R.id.coordinatorLayout),
             getString(R.string.no_ir_text),
-            Snackbar.LENGTH_LONG
+            Snackbar.LENGTH_INDEFINITE
         )
-            .setAction("Action", null)
+            .setAnimationMode(Snackbar.ANIMATION_MODE_FADE)
             .show()
     }
 
@@ -91,13 +100,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @Suppress("DEPRECATION")
     private fun vibrate() {
-        val v = getSystemService(VIBRATOR_SERVICE) as Vibrator
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            v.vibrate(VibrationEffect.createOneShot(150, VibrationEffect.DEFAULT_AMPLITUDE))
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val vibrator = getSystemService(VIBRATOR_MANAGER_SERVICE) as VibratorManager
+            vibrator.defaultVibrator.vibrate(
+                VibrationEffect.createOneShot(
+                    150,
+                    VibrationEffect.DEFAULT_AMPLITUDE
+                )
+            )
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
+            vibrator.vibrate(VibrationEffect.createOneShot(150, VibrationEffect.DEFAULT_AMPLITUDE))
         } else {
-            //deprecated in API 26
-            v.vibrate(150)
+            val vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
+            vibrator.vibrate(150)
         }
     }
 
@@ -113,7 +131,7 @@ class MainActivity : AppCompatActivity() {
             freqMenu.subMenu?.clear()
             var nb = 0
             freqList.forEach {
-                val item = freqMenu.subMenu?.add(0, 100000 + nb, 0, it.toString() + " Hz")
+                val item = freqMenu.subMenu?.add(0, 100000 + nb, 0, "$it Hz")
                 item?.isCheckable = true
                 item?.isChecked = nb == selectedFreq
 
@@ -143,14 +161,7 @@ class MainActivity : AppCompatActivity() {
                     return true
                 }
                 R.id.action_help -> {
-                    val builder = AlertDialog.Builder(this@MainActivity)
-                    builder.setTitle(getString(R.string.dialog_help_title))
-                    builder.setMessage(getString(R.string.dialog_help_message))
-                    builder.setPositiveButton(getString(R.string.dialog_close)) { _, _ ->
-
-                    }
-                    val dialog: AlertDialog = builder.create()
-                    dialog.show()
+                    showHelpDialog()
                 }
             }
         }
@@ -158,25 +169,31 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    private fun showHelpDialog() {
+        val builder = AlertDialog.Builder(this@MainActivity)
+        builder.setTitle(getString(R.string.dialog_help_title))
+        builder.setMessage(getString(R.string.dialog_help_message))
+        builder.setPositiveButton(getString(R.string.dialog_close)) { _, _ -> }
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
     private fun setFrequency(index: Int) {
         val item = freqList.getOrNull(index)
         if (item === null) {
             Snackbar.make(
-                findViewById<View>(R.id.coordinatorLayout),
-                "FREQ NOT FOUND",
+                findViewById(R.id.coordinatorLayout),
+                getString(R.string.freq_not_found),
                 Snackbar.LENGTH_LONG
             )
-                .setAction("Action", null)
                 .show()
             return
         }
 
-        val sharedPref = getPreferences(Context.MODE_PRIVATE)
-        with(sharedPref.edit()) {
+        with(prefs!!.edit()) {
             putInt(getString(R.string.settigns_freq_key), index)
             apply()
         }
-
 
         this.selectedFreq = index
     }
